12505de86a20f0c6f1cbd964b1625a5287d62069
Neeshita <neeshitarathore5415@gmail.com>
2026-07-06 21:34:54 +0530

    debug upload

--- Backend/src/routes/notes.routes.ts
+++ Backend/src/routes/notes.routes.ts
@@ -11,7 +11,11 @@ const notesRoutes = new Hono();
 
 notesRoutes.post("/upload", async (c) => {
     try {
+
+        console.log("Request received");
+
         const body = await c.req.parseBody();
+        console.log("Body parsed");
 
         const file = body.file;
 
@@ -25,38 +29,46 @@ notesRoutes.post("/upload", async (c) => {
             );
         }
 
-        console.log("1. File received");
+        console.log("File received:", file.name);
 
         const filePath = await savePdf(file);
-        console.log("2. File saved");
+        console.log("1. PDF Saved");
 
         const { text, chunks, embeddings } = await processPdf(filePath);
-        console.log("3. PDF processed");
+        console.log("2. PDF Processed");
 
         const document = await saveDocument(
             file.name.replace(".pdf", ""),
             file.name,
             chunks
         );
-        console.log("4. Saved to DB");
+        console.log("3. Document Saved");
 
         await createCollection(embeddings[0].length);
-        console.log("5. Collection ready");
+        console.log("4. Collection Created");
 
-        await storeVectors(document.id, chunks, embeddings);
-        console.log("6. Vectors stored");
+        await storeVectors(
+            document.id,
+            chunks,
+            embeddings
+        );
+        console.log("5. Vectors Stored");
 
         return c.json({
             success: true,
+            message: "PDF uploaded successfully",
+            document,
+            extractedCharacters: text.length,
+            totalChunks: chunks.length,
         });
 
     } catch (err) {
-        console.error(err);
+        console.error("UPLOAD ERROR:", err);
 
         return c.json(
             {
                 success: false,
-                error: err instanceof Error ? err.message : "Unknown error",
+                error: String(err),
             },
             500
         );
